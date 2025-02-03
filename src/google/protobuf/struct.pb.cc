b291fb82bb601044f0e29665d363788c5d44318e
Albert Kharisov <albkharisov@gmail.com>
2025-02-04 00:00:11 +0400

    fixes for esp32 build

--- src/google/protobuf/struct.pb.cc
+++ src/google/protobuf/struct.pb.cc
@@ -44,7 +44,7 @@ struct StructDefaultTypeInternal {
     Struct _instance;
   };
 };
-PROTOBUF_ATTRIBUTE_NO_DESTROY PROTOBUF_CONSTINIT PROTOBUF_ATTRIBUTE_INIT_PRIORITY1 StructDefaultTypeInternal _Struct_default_instance_;
+PROTOBUF_ATTRIBUTE_NO_DESTROY PROTOBUF_ATTRIBUTE_INIT_PRIORITY1 StructDefaultTypeInternal _Struct_default_instance_;
 PROTOBUF_CONSTEXPR Value::Value(
     ::_pbi::ConstantInitialized): _impl_{
     /*decltype(_impl_.kind_)*/{}
